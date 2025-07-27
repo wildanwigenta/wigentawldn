09e75d92c68d08b3575dac877e0082a0265b5ee1
Wildan <wildanwigenta52@gmail.com>
2025-07-27 17:09:40 +0700

    update certificate button

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -46,14 +46,15 @@ const AboutSection = () => {
               </div>
 
               {/* Tombol Lihat Sertifikat */}
-              {/* <div className="mt-6 text-center lg:text-left float-right">
+              <div className="mt-10 text-center lg:text-left float-end">
                 <a
                   href="/certificates"
                   className="inline-block px-6 py-2 text-white bg-gradient-to-r from-accent-cyan to-accent-pink rounded-full shadow-md hover:scale-105 transition-transform duration-300"
                 >
-                  Lihat Sertifikat
+                  My Certificate
                 </a>
-              </div> */}
+              </div>
+
 
   
               {/* Floating decorative elements */}
