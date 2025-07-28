1280c03d8e23befc5b1141329c61ddb6fb19dfd5
Wildan <wildanwigenta52@gmail.com>
2025-07-29 00:16:25 +0700

    clear page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,28 +10,28 @@ export default function Home() {
   return (
     <main className="relative">
       <Navigation />
-      
+
       {/* Main sections */}
       <section id="home">
         <HeroSection />
       </section>
-      
+
       <section id="about">
         <AboutSection />
       </section>
-      
+
       <section id="skills">
         <SkillsSection />
       </section>
-      
+
       <section id="projects">
         <ProjectsSection />
       </section>
-      
+
       <section id="contact">
         <ContactSection />
       </section>
-      
+
       <Footer />
     </main>
   );
