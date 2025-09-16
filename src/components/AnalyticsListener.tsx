ccf42b0cb7b6d43c2ffa2b4ebb285eb8e0e5d0d6
Wildan <wildanwigenta52@gmail.com>
2025-09-17 02:14:19 +0700

    update analytics listener

--- src/components/AnalyticsListener.tsx
+++ src/components/AnalyticsListener.tsx
@@ -6,27 +6,6 @@ import { GA_MEASUREMENT_ID } from "../lib/gtag";
 import * as gtag from "../lib/gtag";
 
 
-// export default function AnalyticsListener() {
-//   useEffect(() => {
-    
-//     console.log("Google Analytics ID di production:", GA_MEASUREMENT_ID);
-
-//     if (typeof window !== "undefined") {
-//       if (typeof window.gtag === "function") {
-//         console.log("window.gtag sudah terdefinisi");
-        
-//         window.gtag("event", "debug_test_event", {
-//           debug_mode: true,
-//         });
-//       } else {
-//         console.warn("window.gtag belum tersedia");
-//       }
-//     }
-//   }, []);
-
-//   return null;
-// }
-
 export default function AnalyticsListener() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
