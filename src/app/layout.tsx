5044b1d81bd6f8b8a4595dbf536c31d17bdd395c
Wildan <wildanwigenta52@gmail.com>
2026-03-12 10:05:25 +0700

    clear layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,6 @@ const plusJakarta = Plus_Jakarta_Sans({
   variable: "--font-plus-jakarta",
   subsets: ["latin"],
 });
-
 export const metadata: Metadata = {
   title: "Wildan Wigenta",
   description:
