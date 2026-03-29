15c16646b006ccf1060acacc108c7f7eba68f135
wildanwigenta <103615474+wildanwigenta@users.noreply.github.com>
2026-03-29 23:17:57 +0700

    feat: Modify metadata in layout.tsx
    Updated metadata for the portfolio website.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ const plusJakarta = Plus_Jakarta_Sans({
   variable: "--font-plus-jakarta",
   subsets: ["latin"],
 });
+
 export const metadata: Metadata = {
   title: "Wildan Wigenta",
   description:
